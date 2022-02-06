1ecf1a05977efe097b07c04aee4dacfcc616784f
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-02-07 00:17:21 +0100

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+function TempCode(){
+  return(
+    <div class="p-5">
+      <header>
+        <nav class="text-primary">
+          <img src="./img/react-logo.png" width="40px"/>
+          <a href="#" class="px-3" >Text</a> 
+          <a href="#" class="px-3" >Sites</a>
+          <a href="#" class="px-3" >Documents</a>
+          <a href="#" class="px-3" >Images</a>
+        </nav>
+      </header>
+      <hr />
+      <body>
+        <h1>That is my first page in <span class="text-primary">React JS</span></h1>
+        <h5>&nbsp;&nbsp;&nbsp;Benefits of <span class="bold">React JS</span></h5>
+        <ul>
+          <li>React is Not A Framework</li>
+          <li>React development And SEO Go Together</li>
+          <li>Virtual DOM is Faster Than Real DOM</li>
+          <li>React Has a Strong Community</li>
+          <li>React is Not Just a Fad</li>
+        </ul>
+      </body>
+      <footer>
+        <small>© 2011–2022 «Yandex»</small>
+      </footer>
+    </div>
+  )
+}
+
+ReactDOM.render(<TempCode /> , document.getElementById("root"))
